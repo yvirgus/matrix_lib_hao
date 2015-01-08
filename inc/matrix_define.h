c7b1c9f04d76c5b88117ee061d6f5a9088ccb0d1
boruoshihao <boruoshihao@gmail.com>
2015-01-08 14:05:13 -0500

    Add matrix_define for acml

--- inc/matrix_define.h
+++ inc/matrix_define.h
@@ -2,16 +2,23 @@
 #define MATRIX_DEFINE
 
 #include "mkl.h"
-
 #define FORTRAN_NAME(x) x
-//#define FORTRAN_NAME(x) x##_
-
-//typedef  int              BL_INT;
-
 typedef  long long        BL_INT;
 typedef  float            BL_FLOAT;     
 typedef  double           BL_DOUBLE;    
 typedef  MKL_Complex8     BL_COMPLEX8;  
 typedef  MKL_Complex16    BL_COMPLEX16; 
 
+
+//#include "acml.h" // change acml.h
+//                  //  %s/complex/ccomplex/g 
+//                  // The function need: cgemm_ dgemm_ sgemm_ zgemm_ zgetrs_ zheev_ 
+//#define FORTRAN_NAME(x) x##_
+//typedef  int              BL_INT;
+//typedef  float            BL_FLOAT;
+//typedef  double           BL_DOUBLE;
+//typedef  ccomplex         BL_COMPLEX8;
+//typedef  doubleccomplex   BL_COMPLEX16;
+
 #endif
+
