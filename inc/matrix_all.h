4b9a951b7c44cbb4be6371979e5c092673b2b5aa
Yudistira Virgus <yudistira.virgus@gmail.com>
2015-05-12 16:47:49 -0400

    * Correct the location of the header files.

--- inc/matrix_all.h
+++ inc/matrix_all.h
@@ -2,10 +2,10 @@
 #define MATRIX_HAO_CLASS_ALL
 
 #include "lib_hao/matrix_base.h"
-#include "matrix_1d.h"
+#include "lib_hao/matrix_1d.h"
 #include "lib_hao/matrix_2d.h"
-#include "matrix_3d.h"
-#include "matrix_element_wise.h"
-#include "lib_hao/matrix_2d_blas_lapack.h"
-#include "matrix_mpi.h"
+#include "lib_hao/matrix_3d.h"
+#include "lib_hao/matrix_element_wise.h"
+/*#include "lib_hao/matrix_2d_blas_lapack.h" */
+#include "lib_hao/matrix_mpi.h"
 #endif
